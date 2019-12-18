333b81f23527dac72c233beecffcc6dd2a637c2b
Andrew Gunnerson <andrewgunnerson@gmail.com>
2019-12-17 22:51:10 -0500

    Add PowerShell script for converting my Beat Saber recordings
    Signed-off-by: Andrew Gunnerson <andrewgunnerson@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Random Scripts
 
 This repo contains random scripts that I use for various purposes.
+
+### `convert-beat-saber-obs-recording.ps1`
+
+Trims my Beat Saber game recordings from OBS (to the nearest H.264 GOP) and fixes audio sync issues caused by SteamVR's mirroring. I use TMPGEnc for lossless (minus starting and ending GOP) frame-perfect trimming for videos I keep.
+
+Dependencies: `ffmpeg`
---
+++ convert-beat-saber-obs-recording.ps1
@@ -0,0 +1,154 @@
+[CmdletBinding(DefaultParameterSetName='noOutPath')]
+param (
+    # Video delay relative to audio
+    [string]$delay = '0.15',
+    # Starting timestamp
+    [Parameter(Mandatory=$true)]
+    [string]$start,
+    # Ending timestamp
+    [string]$end,
+    # Source file
+    [Parameter(Mandatory=$true)]
+    [string]$path,
+    # Target file
+    [parameter(ParameterSetName='haveOutPath')]
+    [string]$outPath,
+    # Target directory
+    [parameter(ParameterSetName='noOutPath')]
+    [string]$outDir = '.',
+    # Date (guess if not provided)
+    [parameter(ParameterSetName='noOutPath')]
+    [ValidatePattern('^\d{4}-\d{2}-\d{2}$')]
+    [string]$date,
+    # Hour (guess if not provided)
+    [parameter(ParameterSetName='noOutPath')]
+    [ValidatePattern('^\d{2}$')]
+    [string]$time,
+    # Artist
+    [Parameter(ParameterSetName='noOutPath')]
+    [string]$artist,
+    # Song
+    [Parameter(ParameterSetName='noOutPath', Mandatory=$true)]
+    [string]$song,
+    # Level type
+    [Parameter(ParameterSetName='noOutPath', Mandatory=$true)]
+    [ValidatePattern('^((90|360)° )?(Easy|Normal|Hard|Expert\+?)$')]
+    [string]$difficulty,
+    # Misses
+    [Parameter(ParameterSetName='noOutPath', Mandatory=$true)]
+    [int]$misses,
+    # Rank
+    [Parameter(ParameterSetName='noOutPath', Mandatory=$true)]
+    [ValidatePattern('^([A-F]|S{1,3})$')]
+    [string]$rank
+)
+
+$ErrorActionPreference = 'Stop'
+
+###
+
+Add-Type `
+    -Name Native `
+    -Namespace $null `
+    -MemberDefinition @"
+[StructLayout(LayoutKind.Sequential)]
+public struct BY_HANDLE_FILE_INFORMATION
+{
+    public UInt32 FileAttributes;
+    public System.Runtime.InteropServices.ComTypes.FILETIME CreationTime;
+    public System.Runtime.InteropServices.ComTypes.FILETIME LastAccessTime;
+    public System.Runtime.InteropServices.ComTypes.FILETIME LastWriteTime;
+    public UInt32 VolumeSerialNumber;
+    public UInt32 FileSizeHigh;
+    public UInt32 FileSizeLow;
+    public UInt32 NumberOfLinks;
+    public UInt32 FileIndexHigh;
+    public UInt32 FileIndexLow;
+};
+
+[DllImport("kernel32.dll", SetLastError = true)]
+private static extern bool GetFileInformationByHandle(IntPtr hFile, out BY_HANDLE_FILE_INFORMATION lpFileInformation);
+
+public static BY_HANDLE_FILE_INFORMATION GetFileInformation(string path) {
+    using (var f = System.IO.File.OpenRead(path)) {
+        BY_HANDLE_FILE_INFORMATION info;
+        GetFileInformationByHandle(f.SafeFileHandle.DangerousGetHandle(), out info);
+        return info;
+    }
+}
+"@
+
+function Test-PathsEqual {
+    [CmdletBinding()]
+    param (
+        [string]$path1,
+        [string]$path2
+    )
+
+    try {
+        $info1 = [Native]::GetFileInformation((Resolve-Path -Path $path1).Path)
+        $info2 = [Native]::GetFileInformation((Resolve-Path -Path $path2).Path)
+
+        return $info1.VolumeSerialNumber -eq $info2.VolumeSerialNumber -and
+                $info1.FileIndexLow -eq $info2.FileIndexLow -and
+                $info1.FileIndexHigh -eq $info2.FileIndexHigh
+    } catch [System.Management.Automation.ItemNotFoundException] {
+        return $false
+    }
+}
+
+###
+
+if (!$outPath) {
+    $baseName = Split-Path -Path $path -Leaf
+
+    if ($baseName -match '^(\d{4}-\d{2}-\d{2})\s*-?\s*(\d{2})') {
+        $date = $matches[1]
+        $time = $matches[2]
+    }
+
+    if (!$date -or !$time) {
+        throw 'Date or time not provided and could not be determined from input filename'
+    }
+
+    $missesStr = switch ($misses) {
+        { $_ -lt 0 } { 'Failed' }
+        0 { 'Full Combo' }
+        1 { '1 miss' }
+        default { "$_ misses" }
+    }
+
+    $artistStr = if ($artist) {
+        "$artist - "
+    } else {
+        ""
+    }
+
+    $outPath = Join-Path $outDir "$date - $time - $artistStr$song ($difficulty) - $missesStr - $rank.mkv"
+}
+
+if (Test-PathsEqual $path $outPath) {
+    throw 'Input and output paths are the same'
+}
+
+$timestampArgs = @('-ss', $start)
+if ($end) {
+    $timestampArgs += @('-to', $end)
+}
+
+$args =
+    $timestampArgs +
+    @('-i', $path) +
+    $timestampArgs +
+    @(
+        '-itsoffset', $delay,
+        '-i', $path,
+        '-map', '1:v',
+        '-map', '0:a',
+        '-c', 'copy',
+        '-avoid_negative_ts', '1',
+        $outPath
+    )
+
+ffmpeg.exe @args
+exit $LASTEXITCODE

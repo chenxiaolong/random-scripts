44cd571abc85c3a21237748b20dafeec98f2c4b1
Andrew Gunnerson <chillermillerlong@hotmail.com>
2020-12-30 19:26:18 -0500

    convert-beat-saber-obs-recording.ps1: Recognize Lawless in level type
    Signed-off-by: Andrew Gunnerson <chillermillerlong@hotmail.com>

--- convert-beat-saber-obs-recording.ps1
+++ convert-beat-saber-obs-recording.ps1
@@ -33,7 +33,7 @@ param (
     [string]$song,
     # Level type
     [Parameter(ParameterSetName='noOutPath', Mandatory=$true)]
-    [ValidatePattern('^(((90|360)°|One Handed) )?(Easy|Normal|Hard|Expert\+?)$')]
+    [ValidatePattern('^(((90|360)°|One Handed|Lawless) )?(Easy|Normal|Hard|Expert\+?)$')]
     [string]$difficulty,
     # Misses
     [Parameter(ParameterSetName='noOutPath', Mandatory=$true)]
